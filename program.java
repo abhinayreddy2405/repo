df9397e81059548d95b216bfa6529d578e8a22df
abhinay <abhinayreddy245@gmail.com>
2024-12-15 19:28:17 +0530

    this files are added to repo

---
+++ program.java
@@ -0,0 +1,21 @@
+public class program {
+    public static void main(String[] args) {
+        // Predefined string to check
+        String input = "A man, a plan, a canal, Panama";
+       
+        // Normalize the input string by removing spaces and converting to lowercase
+        String normalizedString = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+       
+        // Reverse the normalized string
+        String reversedString = new StringBuilder(normalizedString).reverse().toString();
+       
+        // Check if the normalized string is equal to its reversed version
+        if (normalizedString.equals(reversedString)) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
+    }
+}
+
+
