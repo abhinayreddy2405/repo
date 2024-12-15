7f557f80dad1844cbaca87030f5e4358e8f1177a
abhinay <abhinayreddy245@gmail.com>
2024-12-15 19:51:16 +0530

    added

--- program.java
+++ program.java
@@ -1,14 +1,14 @@
-public class prograim {
+public class program {
     public static void main(String[] args) {
         // Predefined string to check
         String input = "level";
-       
+
         // Normalize the input string by removing spaces and converting to lowercase
         String normalizedString = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-       
+
         // Reverse the normalized string
         String reversedString = new StringBuilder(normalizedString).reverse().toString();
-       
+
         // Check if the normalized string is equal to its reversed version
         if (normalizedString.equals(reversedString)) {
             System.out.println("\"" + input + "\" is a palindrome.");
@@ -17,5 +17,6 @@ public class prograim {
         }
     }
 }
+
 
 
